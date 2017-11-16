2143ac46ec50c99c9147cd4c07807d8135e59374
fabho992 <fabho992@RUD-D11015>
2017-11-16 16:40:12 +0100

    updated all flavors accordingly

--- Techs/TechsFlavors.sql
+++ Techs/TechsFlavors.sql
@@ -46,3 +46,28 @@ DELETE FROM Technology_Flavors WHERE TechType = 'TECH_CONSTRUCTION' AND FlavorTy
 UPDATE Builds SET PrereqTech = 'TECH_WOOD_WORKING' WHERE Type = 'BUILD_LUMBERMILL';
 UPDATE Builds SET PrereqTech = 'TECH_FISHING' WHERE Type = 'BUILD_FISHING_BOATS';
 UPDATE Builds SET PrereqTech = 'TECH_HUNTING' WHERE Type = 'BUILD_CAMP';
+
+-----------------------------------------------------------------------
+--	VP: POTTERY
+-----------------------------------------------------------------------
+--	removing reglious flavor
+DELETE FROM Technology_Flavors WHERE TechType = 'TECH_POTTERY' AND FlavorType = 'FLAVOR_RELIGION' 
+AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_BUILDINGS' AND Value= 1 );
+
+-----------------------------------------------------------------------
+--	VP: MYSTICISM
+-----------------------------------------------------------------------
+--	increase religous flavor of mysticism from 10 to 15 (it is assumed 10 from vanilla civ)
+UPDATE Technology_Flavors SET Flavor = 15 WHERE TechType = 'TECH_MYSTICISM' AND FlavorType = 'FLAVOR_RELIGION';
+
+-----------------------------------------------------------------------
+--	VP: TRADE (TECH_HORSEBACK_RIDING)
+-----------------------------------------------------------------------
+--	surprisingly not in vp
+INSERT INTO Technology_Flavors (TechType, FlavorType, Flavor) VALUES('TECH_HORSEBACK_RIDING','FLAVOR_GOLD', 10);
+
+-----------------------------------------------------------------------
+--	VP: ARCHERY
+-----------------------------------------------------------------------
+--	artemites now moved to archery so add wonder flavor
+INSERT INTO Technology_Flavors (TechType, FlavorType, Flavor) VALUES('TECH_ARCHERY','FLAVOR_WONDER', 2);
