fc9a9a41f3125a50a2ca80a0c48b78bbbef012f8
DESKTOP-E6PB4L5\Zakariel <zakariel76@gmail.com>
2017-11-25 06:58:49 +0100

    setting wheat to immediate reveal

--- Build/Ancient Techs Rebalanced - Vox Populi/Techs/TechsFlavors.sql
+++ Build/Ancient Techs Rebalanced - Vox Populi/Techs/TechsFlavors.sql
@@ -53,7 +53,9 @@ UPDATE Builds SET PrereqTech = 'TECH_HUNTING' WHERE Type = 'BUILD_CAMP';
 -----------------------------------------------------------------------
 --===================================================================--
 
-UPDATE Builds SET PrereqTech = 'TECH_HORSEBACK_RIDING' WHERE Type = 'BUILD_TRADING_POST' AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_TERRAIN' AND Value= 1 );
+UPDATE Builds
+SET PrereqTech = 'TECH_POTTERY'
+WHERE Type = 'BUILD_TRADING_POST' AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_TERRAIN' AND Value= 1 );
 
 UPDATE Builds
 SET PrereqTech = 'TECH_MINING'
