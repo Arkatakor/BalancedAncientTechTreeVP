e5c052ab662a00fdcfef3c1351b7c83587f8480f
DESKTOP-E6PB4L5\Zakariel <zakariel76@gmail.com>
2017-12-20 19:16:15 +0100

    trading post set to 1 gold for pottery, 3 culture and 1 culture for currency.

--- BalancedAncientTechTreeVP/Improvements/Improvements.sql
+++ BalancedAncientTechTreeVP/Improvements/Improvements.sql
@@ -15,3 +15,19 @@ UPDATE Builds SET PrereqTech = 'TECH_HUNTING' WHERE Type = 'BUILD_CAMP';
 -----------------------------------------------------------------------
 UPDATE Builds SET PrereqTech = 'TECH_POTTERY' WHERE Type = 'BUILD_TRADING_POST';
 
+--===================================================================--
+-----------------------------------------------------------------------
+-- 					VP UPDATES 
+-----------------------------------------------------------------------
+--===================================================================--
+UPDATE Improvement_Yields SET Yield = '1' WHERE YieldType = 'YIELD_GOLD' AND ImprovementType = 'IMPROVEMENT_TRADING_POST' 
+AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_TERRAIN' AND Value= 1 );
+
+UPDATE Improvement_Yields SET Yield = '0' WHERE YieldType = 'YIELD_CULTURE' 
+AND ImprovementType = 'IMPROVEMENT_TRADING_POST' AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_TERRAIN' AND Value= 1 );
+
+INSERT INTO Improvement_TechYieldChanges
+	(ImprovementType, TechType, YieldType, Yield)
+VALUES
+	('IMPROVEMENT_TRADING_POST', 'TECH_CURRENCY', 'YIELD_CULTURE', 1),
+	('IMPROVEMENT_TRADING_POST', 'TECH_CURRENCY', 'YIELD_GOLD', 2);
