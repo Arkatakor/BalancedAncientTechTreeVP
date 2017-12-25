63f4e5c45f927597c6149b6ab775c1b13ef209c6
Arkatakor <zakariel76@gmail.com>
2017-12-25 09:38:45 +0100

    pathfinder set to archery.  light axeman upgrades to archer

--- BalancedAncientTechTreeVP/Units/Updates.sql
+++ BalancedAncientTechTreeVP/Units/Updates.sql
@@ -11,7 +11,7 @@ UPDATE Units SET PrereqTech = 'TECH_HUNTING' WHERE Class = 'UNITCLASS_SCOUT';
 -----------------------------------------------------------------------
 --	HUNTING
 -----------------------------------------------------------------------
-UPDATE Units SET PrereqTech = 'TECH_HUNTING' WHERE Class = 'UNITCLASS_PATHFINDER'
+UPDATE Units SET PrereqTech = 'TECH_ARCHERY' WHERE Class = 'UNITCLASS_PATHFINDER'
 AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_UNITS' AND Value= 1 );
 
 -----------------------------------------------------------------------
